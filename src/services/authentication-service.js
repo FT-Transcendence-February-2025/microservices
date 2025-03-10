ca07e277c3141457ba4338dca37eb19e5e0b381c
cptfran <frankiszymon@gmail.com>
2025-03-10 21:23:27 +0100

    Feat: user user sessions User is recognized from different browser/devices. Further will be developed to recognize incognito mode. Needs to be done from frontend.

--- src/services/authentication-service.js
+++ src/services/authentication-service.js
@@ -3,13 +3,14 @@ import jwt from "jsonwebtoken";
 import db from "./database-service.js";
 import * as crypto from "crypto";
 
+
 const authenticationService = async (email, password, userAgent) => {
 	const user = await db.getUserByEmail(email);
 	if (user.error) {
     return { status: 500, error: "Internal Server Error" };
 	}
   if (!user) {
-    return { status: 404, error: "User not found" };
+    return { status: 404, error: "User not found" };5
   }
 
   const isPasswordValid = await fastify.bcrypt.compare(password, user.password);
@@ -36,11 +37,11 @@ const authenticationService = async (email, password, userAgent) => {
 		const deviceHash = crypto.createHash('sha256').update(userAgent).digest('hex');
     const expiresAt = Math.floor(Date.now() / 1000) + expiresInSeconds;
 		const device = await db.getDevice(deviceHash);
-		if (device.error) {
+		if (device && device.error) {
       return { status: 500, error: "Internal Server Error" };
 		}
 		if (!device) {
-			const addResult = await db.addDevice(user.id);
+			const addResult = await db.addDevice(user.id, deviceHash, refreshToken, expiresAt);
 			if (addResult.error) {
         return { status: 500, error: "Internal Server Error" };
 			}

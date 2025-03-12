1af508a98a61a14502098351c57488b7a22e22f1
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-03-12 15:48:08 +0100

    Squashed 'services/match-service/' content from commit d846a31
    git-subtree-dir: services/match-service
    git-subtree-split: d846a31644c8eb89f3dad00f56610e527db87f54

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch';
+
+const AUTH_SERVICE_URL = 'http://authentication:3000';
+
+export class User {
+  static async findById(id) {
+    try {
+      const response = await fetch(`${AUTH_SERVICE_URL}/api/users/${id}`);
+      if (!response.ok) return null;
+      const userData = await response.json();
+      return userData;
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      return null;
+    }
+  }
+
+  static async findByEmail(email) {
+    try {
+      const response = await fetch(
+        `${AUTH_SERVICE_URL}/api/users/email/${email}`
+      );
+      if (!response.ok) return null;
+      const userData = await response.json();
+      return userData;
+    } catch (error) {
+      console.error('Error fetching user:', error);
+      return null;
+    }
+  }
+}

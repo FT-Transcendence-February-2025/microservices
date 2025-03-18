917fb9c598dcf7c4dadc20b19103e847a5793773
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-03-18 21:59:38 +0100

    [Fix]: Api redirection working front-end to auth service.

--- services/authentication-service/src/src/routes/email-route.js
+++ services/authentication-service/src/src/routes/email-route.js
@@ -1,28 +1,28 @@
 import emailController from "../controllers/email-controller.js";
 import verifyToken from "jwt-validator-tr";
 
-const emailRoute = {
-	method: "POST",
-	url: "/email",
-	schema: {
-		body: {
-			type: "object",
-			properties: {
-				email: { type: "string" }
-			},
-			required: ["email"]
-		},
-		response: {
-			200: {
-				type: "object",
-				properties: {
-					success: { type: "string" }
-				}
-			}
-		}
-	},
-	preHandler: verifyToken,
-	handler: emailController
-};
-
-export default emailRoute;
+export default async function (fastify, opts) {
+  fastify.route({
+    method: "POST",
+    url: "/email",
+    schema: {
+      body: {
+        type: "object",
+        properties: {
+          email: { type: "string" }
+        },
+        required: ["email"]
+      },
+      response: {
+        200: {
+          type: "object",
+          properties: {
+            success: { type: "string" }
+          }
+        }
+      }
+    },
+    preHandler: verifyToken, // Middleware to verify token before the handler
+    handler: emailController
+  });
+}

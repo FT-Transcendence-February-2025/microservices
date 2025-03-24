a99c4ab07e9568bacfab6fb4913fcfae39ed1983
Manuel Guillermo Cruz Sales <mcruz-sa@student.42wolfsburg.de>
2025-03-24 13:14:20 +0100

    Feat: updated to update match results when game ends

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { userRoutes } from './routes/users.js'
 import { matchmakingDbRoute } from './routes/database_route.js'
 import { websocketHandler } from './websocket/index.js'
 import { initDatabase } from './db/schema.js'
+import { matchResultsRoute } from './routes/match_routes.js'
 
 const PORT = 3000
 
@@ -41,6 +42,7 @@ fastify.get('/', (request, reply) => {
 fastify.register(userRoutes)
 fastify.register(websocketHandler)
 fastify.register(matchmakingDbRoute)
+fastify.register(matchResultsRoute)
 
 // Server
 const start = async () => {

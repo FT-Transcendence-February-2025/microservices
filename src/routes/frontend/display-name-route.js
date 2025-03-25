f68677ba88550b96c8ae4b9faa511c4c9d266e8c
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-03-25 20:59:09 +0100

    Squashed 'services/user-mgmt-service/src/' changes from 74e15aa..4ca318c
    4ca318c Merge pull request #9 from FT-Transcendence-February-2025/avatar
    63bd903 refactor: matchmaking route change To fit into Traefic service.
    3336fb4 feat: avatar view Created avatar view route and distingishued avatar change route. Modified routes so they serve correctly to Traeffic service.
    
    git-subtree-dir: services/user-mgmt-service/src
    git-subtree-split: 4ca318ce5db6e49712be54305a51585e75d3643d

--- src/routes/frontend/display-name-route.js
+++ src/routes/frontend/display-name-route.js
@@ -1,28 +1,28 @@
 import frontendController from "../../controllers/frontend-controller.js";
 import jwtTr from "jwt-validator-tr";
 
-const displayNameRoute = {
-	method: "POST",
-	url: "/display-name",
-	schema: {
-		body: {
-			type: "object",
-			properties: {
-				displayName: { type: "string" }
-			},
-			required: ["displayName"]
-		},
-		response: {
-			200: {
+export default async function (fastify, opts) {
+	fastify.route({
+		method: "POST",
+		url: "/display-name",
+		schema: {
+			body: {
 				type: "object",
 				properties: {
-					success: { type: "string" }
+					displayName: { type: "string" }
+				},
+				required: ["displayName"]
+			},
+			response: {
+				200: {
+					type: "object",
+					properties: {
+						success: { type: "string" }
+					}
 				}
 			}
-		}
-	},
-	preHandler: jwtTr.verifyAccessToken,
-	handler: frontendController.displayName
+		},
+		preHandler: jwtTr.verifyAccessToken,
+		handler: frontendController.displayName	
+	});
 };
-
-export default displayNameRoute;

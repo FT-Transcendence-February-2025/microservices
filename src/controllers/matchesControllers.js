adb9ba8afc8fd1743b20dfca413df2e898bf9f83
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-03-25 16:06:58 +0100

    Feat: update matchesController to updateUserStats and send the payload to user management

--- src/controllers/matchesControllers.js
+++ src/controllers/matchesControllers.js
@@ -1,17 +1,17 @@
 import db from '../db/connection.js'
 
-async function updateUserStats (userId, won) {
+async function updateUserStats (stats) {
   const endpoint = 'http://localhost:3002/updateStats'
   try {
     const response = await fetch(endpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userId, won })
+      body: JSON.stringify(stats)
     })
     return await response.json()
   } catch (error) {
-    console.error('Error updating user stats:', error)
-    return { error: 'Update failed' }
+    console.error('Error updating user statistics:', error)
+    return { error: 'Statistics update failed' }
   }
 }
 
@@ -39,18 +39,35 @@ export const matchesController = {
       stmt.run(player1Score, player2Score, winnerId, matchId)
 
       const match = db.prepare(`
-        SELECT player1_id, player2_id
+        SELECT player1_id, player2_id, created_at, started_at, ended_at
         FROM matchmaking
         WHERE id = ?
       `).get(matchId)
       const loserId = match.player1_id === winnerId ? match.player2_id : match.player1_id
 
-      const winUpdate = await updateUserStats(winnerId, true)
-      const lossUpdate = await updateUserStats(loserId, false)
+      const winnerStats = {
+        userId: winnerId,
+        won: true,
+        opponentId: loserId,
+        score: { win: player1Score > player2Score ? player1Score : player2Score },
+        matchDate: match.ended_at,
+        matchId
+      }
+
+      const loserStats = {
+        userId: loserId,
+        won: false,
+        opponentId: winnerId,
+        score: { loss: player1Score < player2Score ? player1Score : player2Score },
+        matchDate: match.ended_at,
+        matchId
+      }
+      const winUpdate = await updateUserStats(winnerStats)
+      const lossUpdate = await updateUserStats(loserStats)
 
       return reply.code(200).send({
         success: true,
-        message: 'Match result saved successfully',
+        message: 'Match result and user statistics updated successfully',
         winUpdate,
         lossUpdate
       })

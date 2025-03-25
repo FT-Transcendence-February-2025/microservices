a674598fc611798148247617235cacb11f9d79aa
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-03-25 18:15:44 +0100

    Fix: postMatchResults method to align with user-management schema

--- src/controllers/matchesControllers.js
+++ src/controllers/matchesControllers.js
@@ -45,22 +45,29 @@ export const matchesController = {
       `).get(matchId)
       const loserId = match.player1_id === winnerId ? match.player2_id : match.player1_id
 
+      let winnerScore, loserScore
+      if (match.player1_id === winnerId) {
+        winnerScore = player1Score
+        loserScore = player2Score
+      } else {
+        winnerScore = player2Score
+        loserScore = player1Score
+      }
+
       const winnerStats = {
         userId: winnerId,
-        won: true,
         opponentId: loserId,
-        score: { win: player1Score > player2Score ? player1Score : player2Score },
-        matchDate: match.ended_at,
-        matchId
+        userScore: winnerScore,
+        opponentScore: loserScore,
+        matchDate: match.ended_at
       }
 
       const loserStats = {
         userId: loserId,
-        won: false,
         opponentId: winnerId,
-        score: { loss: player1Score < player2Score ? player1Score : player2Score },
-        matchDate: match.ended_at,
-        matchId
+        userScore: loserScore,
+        opponentScore: winnerScore,
+        matchDate: match.ended_at
       }
       const winUpdate = await updateUserStats(winnerStats)
       const lossUpdate = await updateUserStats(loserStats)

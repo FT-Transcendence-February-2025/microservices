07a7f5b783fdf3c7e4efe8d1de68aab514568818
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-04-06 18:30:11 +0200

    [Fix]: match server file correction after merge

--- services/match-service/src/src/server.js
+++ services/match-service/src/src/server.js
@@ -1,23 +1,14 @@
 import Fastify from 'fastify'
 import fastifyCors from '@fastify/cors'
 import fastifyWebsocket from '@fastify/websocket'
-<<<<<<< HEAD
-import { userRoutes } from './routes/users.js'
-=======
->>>>>>> 23374cfc189bf63826a3422aa8f875beed69671a
 import { matchmakingDbRoute } from './routes/database_route.js'
 import { websocketHandler } from './websocket/index.js'
 import { initDatabase } from './db/schema.js'
 import matchesRoute from './routes/match_routes.js'
 import matchmakingRoute from './routes/matchmaking-route.js'
-<<<<<<< HEAD
-
-const PORT = 3003
-=======
 import tournamentRoute from './routes/tournament_route.js'
 
 const PORT = 3000
->>>>>>> 23374cfc189bf63826a3422aa8f875beed69671a
 
 const fastify = Fastify({
   logger: true
@@ -33,11 +24,7 @@ try {
 }
 
 await fastify.register(fastifyCors, {
-<<<<<<< HEAD
-  origin: ['http://user:3000', 'http://localhost:3000'],
-=======
   origin: ['http://user-management:3000', 'http://localhost:3000'],
->>>>>>> 23374cfc189bf63826a3422aa8f875beed69671a
   methods: ['GET', 'POST', 'PUT'],
   credentials: true
 })
@@ -45,32 +32,18 @@ await fastify.register(fastifyCors, {
 // Register WebSocket
 await fastify.register(fastifyWebsocket)
 
-<<<<<<< HEAD
-// Test get route to send reply message
-=======
->>>>>>> 23374cfc189bf63826a3422aa8f875beed69671a
 fastify.get('/', (request, reply) => {
   reply.send({
     message: 'Hello Fastify. Server is running!'
   })
 })
 
-<<<<<<< HEAD
-// Routes
-fastify.register(userRoutes)
-=======
->>>>>>> 23374cfc189bf63826a3422aa8f875beed69671a
 fastify.register(websocketHandler)
 fastify.register(matchmakingDbRoute)
 fastify.register(matchesRoute, { prefix: '/matches' })
 fastify.register(matchmakingRoute)
-<<<<<<< HEAD
-
-// Server
-=======
 fastify.register(tournamentRoute, { prefix: '/tournament' })
 
->>>>>>> 23374cfc189bf63826a3422aa8f875beed69671a
 const start = async () => {
   try {
     await fastify.listen({ port: PORT, host: '0.0.0.0' })

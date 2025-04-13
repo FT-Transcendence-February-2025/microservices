91a9037033c1f5cfb813fb19b1e619a59af3dc0f
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-04-13 16:22:37 +0200

    Feat: adding verifyAccessToken method from jwtTr package

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,5 +1,14 @@
 import { connectionHandler } from './handlers/connection.js'
+import jwtTr from 'jwt-validator-tr'
 
 export const websocketHandler = (fastify) => {
-  fastify.get('/ws', { websocket: true }, connectionHandler)
+  fastify.get('/ws', {
+    websocket: true,
+    preHandler: async (request, reply) => {
+      if (!request.headers.authorization && request.query.token) {
+        request.headers.authorization = `Bearer ${request.query.token}`
+      }
+      await jwtTr.verifyAccessToken(request, reply)
+    }
+  }, connectionHandler)
 }

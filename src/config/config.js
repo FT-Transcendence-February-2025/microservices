b751b0859216086b17d07ba3707ee7b4bb9552fa
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-04-13 21:39:45 +0200

    [fix]: using import instead of require for config.js * Added gmail security codes

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
-const fs = require('fs');
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+dotenv.config();
 const isDocker = fs.existsSync('/.dockerenv');
 
 const config = {
   endpoints: {
     auth: isDocker ? 'http://auth:3001' : 'http://localhost:3001',
     user: isDocker ? 'http://user:3002' : 'http://localhost:3002',
-    tour: isDocker ? 'http://tour:3003' : 'http://localhost:3003',
-    match: isDocker ? 'http://match:3004' : 'http://localhost:3004',
-    game: isDocker ? 'http://game:3005' : 'http://localhost:3005'
+    tour: isDocker ? 'http://match:3003' : 'http://localhost:3003',
+    match: isDocker ? 'http://tour:3004' : 'http://localhost:3004',
+    game: isDocker ? 'http://game:3005' : 'http://localhost:3005',
   },
   apiPrefix: isDocker ? '/api' : '',
   logger: isDocker
@@ -22,17 +24,7 @@ const config = {
         },
       }
     : true,
-  // CORS configuration centralized here:
-  cors: {
-    origin: [
-      `https://${process.env.DOMAIN}`,
-      `http://auth.${process.env.DOMAIN}`,
-      'localhost'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  },
   isDocker,
 };
 
-module.exports = config;
+export default config;

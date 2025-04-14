1db4975f16e35ce7afba40744922430165a99c39
Guillermo Sales <guillermosales@MBP-de-Jimena.fritz.box>
2025-04-14 13:13:44 +0200

    Add: adding MatchMaking singleton

--- src/components/menu/queue/queue.ts
+++ src/components/menu/queue/queue.ts
@@ -1,6 +1,6 @@
 import queueTemplate from './queue.html?raw';
 import { router } from '../../../index';
-import { webSocketConnection } from '../../../services/webSocketHandler';
+import MatchmakingSocket from '../../../services/matchmakingSocket';
 
 const template = document.createElement('template');
 template.innerHTML = queueTemplate;
@@ -19,55 +19,57 @@ export default class Queue extends HTMLElement {
     }
 
     connectedCallback(): void {
-        this._ws = webSocketConnection({
-            onOpen: (event) => {
-                console.log("WebSocket onOpen callback fired:", event);
-                const token = localStorage.getItem('accessToken');
-                this._ws?.send(JSON.stringify({ type: 'joinQueue', token }));
-            },
-            onMessage: (event) => {
-                const data = JSON.parse(event.data);
-                console.log("WebSocket callback received:", data);
-                switch (data.type) {
-                    case 'joinQueue':
-                        console.log("Successfully joined the queue");
-                        break;
-                    case 'matchCreated':
-                        this.updateUIForMatchCreated(data);
-                        break;
-                    case 'matchAccepted':
-                        console.log("Match accepted:", data.message);
-                        break;
-                    case 'matchStarted':
-                        console.log("Match started. Redirecting to game URL:", data.gameUrl);
-                        window.location.href = data.gameUrl;
-                        break;
-                    case 'leaveQueue':
-                        console.log("Left the queue");
-                        break;
-                    case 'matchCancelled':
-                        console.log("Match Cancelled")
-                        this.displayNotification('Your match has been cancelled. Please try again later.');
-                        setTimeout(() => { router.navigateTo('/play')}, 2000);
-                        break;
-                    default:
-                        console.warn("Received unknown message type:", data.type);
-                        break;
+        this._ws = MatchmakingSocket.getInstance();
+        this._ws.onopen = (event) => {
+            console.log("Queue Global socket onOpen", event);
+            const token = localStorage.getItem('accessToken');
+            this._ws?.send(JSON.stringify({ type: 'joinQueue', token }));
+        };
+        this._ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log("WebSocket callback received:", data);
+            switch (data.type) {
+                case 'joinQueue':
+                    console.log("Successfully joined the queue");
+                    break;
+                case 'matchCreated':
+                    this.updateUIForMatchCreated(data);
+                    break;
+                case 'matchAccepted':
+                    console.log("Match accepted:", data.message);
+                    break;
+                case 'matchStarted':
+                    console.log("Match started. Redirecting to game URL:", data.gameUrl);
+                    // window.location.href = data.gameUrl;
+                    router.navigateTo(data.gameUrl);
+                    break;
+                case 'leaveQueue':
+                    console.log("Left the queue");
+                    break;
+                case 'matchCancelled':
+                    console.log("Match Cancelled")
+                    this.displayNotification('Your match has been cancelled. Please try again later.');
+                    setTimeout(() => { router.navigateTo('/play') }, 2000);
+                    break;
+                default:
+                    console.warn("Received unknown message type:", data.type);
+                    break;
                 }
             }
-        });
-        // Attach a listener to the leave link to send the WS message
-        const leaveLink = this._card.querySelector('#leaveLink') as HTMLAnchorElement;
-        leaveLink.addEventListener('click', (event: Event) => {
-            event.preventDefault();
-            if (this._ws && this._ws.readyState === WebSocket.OPEN) {
-                this._ws.send(JSON.stringify({ type: 'leaveQueue' }));
-            } else {
-                console.error("WebSocket connection is not available");
-            }
-            router.navigateTo('/play');
-        });
-    }
+
+            // Attach a listener to the leave link to send the WS message
+            const leaveLink = this._card.querySelector('#leaveLink') as HTMLAnchorElement;
+            leaveLink.addEventListener('click', (event: Event) => {
+                event.preventDefault();
+                if (this._ws && this._ws.readyState === WebSocket.OPEN) {
+                    this._ws.send(JSON.stringify({ type: 'leaveQueue' }));
+                } else {
+                    console.error("WebSocket connection is not available");
+                }
+                router.navigateTo('/play');
+            });   
+        };
+    
 
     private updateUIForMatchCreated(data: { matchId: string, gameUrl?: string }): void {
         this._card.innerHTML = `

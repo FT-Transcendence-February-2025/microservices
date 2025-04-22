30b9ccc12f4f76227a8e8e52f8aa7fe26cbcab7a
VictoriaLizCor <victoria.lizcor@gmail.com>
2025-04-22 09:57:55 +0200

    [Fix]: commented output for generate secrets

--- scripts/submodules.mk
+++ scripts/submodules.mk
@@ -20,6 +20,7 @@ subStatus:
 	git submodule foreach 'echo "$$path:"; git fetch; git status'
 
 # git submodule foreach 'git push -u origin feature-branch'
-#git submodule foreach --recursive 'git push -u origin microservices-integration'
+switch:
+	git submodule foreach --recursive 'git checkout microservices-integration'
 
 #git submodule update --init --recursive
